2ba9bafe504407e5b63573b1bc1ca9f46da8ed84
Anastasia Skoryukova <anastasia_skoryukova@mail.ru>
2020-06-06 22:38:51 +0300

    The first version without the custom textfields

---
+++ Currency Converter/CurConTextField.swift
@@ -0,0 +1,15 @@
+//
+//  CurConTextField.swift
+//  Currency Converter
+//
+//  Created by Анастасия on 03.06.2020.
+//  Copyright © 2020 Anastasia Skoryukova. All rights reserved.
+//
+
+import UIKit
+
+class CurConTextField: UITextField {
+
+ 
+
+}
