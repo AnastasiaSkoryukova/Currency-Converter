56133ac776e1a542da90162732aee1bb10aad756
Anastasia Skoryukova <anastasia_skoryukova@mail.ru>
2020-06-23 14:02:15 +0300

    Network call using network manager is added and up to date. The server adress is added to the exceptions.

---
+++ Currency Converter/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Currency Converter
+//
+//  Created by Анастасия on 20.06.2020.
+//  Copyright © 2020 Anastasia Skoryukova. All rights reserved.
+//
+
+import Foundation
